0183f826a1f531f7ab789071e4d2453d4a13e6a0
kogicko240 <dmn120112@gmail.com>
2026-05-06 17:56:47 +0300

    Task05 Done 17:57

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -4,3 +4,45 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет сумму элементов вектора, расположенных
 // между первыми максимальным по модулю и минимальным по модулю элементами.
+#include "logic.h"
+#include <cmath>
+
+int sum_between_first_min_max(int* array, int size) {
+	if (array == nullptr || size < 2) {
+		return 0;
+	}
+	int maxAbsIndex = 0;
+	int maxAbsValue = abs(array[0]);
+
+	for (int i = 0; i < size; i++) {
+		if (abs(array[i]) > maxAbsValue) {
+			maxAbsValue = abs(array[i]);
+			maxAbsIndex = i;
+		}
+	}
+	int minAbsIndex = 0;
+	int minAbsValue = abs(array[0]);
+
+	for (int i = 1; i < size; i++) {
+		if (abs(array[i]) < minAbsValue) {
+			minAbsValue = abs(array[i]);
+			minAbsIndex = i;
+		}
+	}
+	int left = maxAbsIndex;
+	int right = minAbsIndex;
+
+	if (left > right) {
+		int temp = left;
+		left = right;
+		right - temp;
+	}
+	if (right - left <= 1) {
+		return 0;
+	}
+	int sum = 0;
+	for (int i = left + 1; i < right; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
